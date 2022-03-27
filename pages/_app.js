e4b562b0d326a7e1f0f03c780cff37ad5aed57d4
Junior Alves <jrnalves@gmail.com>
2022-03-27 17:38:11 -0300

    create Global Styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import { ThemeProvider } from 'styled-components'
 import Reset from '../styles/Reset'
+import GlobalStyles from '../styles/GlobalStyles'
 
 const theme = {
   colors: {
@@ -12,6 +13,7 @@ export default function App({ Component, pageProps }) {
     <>
       <Reset />
       <ThemeProvider theme={theme}>
+        <GlobalStyles />
         <Component {...pageProps} />
       </ThemeProvider>
     </>
---
+++ styles/GlobalStyles.js
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+    html, body {
+        font-family: 'Roboto', sans-serif;
+        font-size: 14px;
+    }
+`;
+
+export default GlobalStyles;
