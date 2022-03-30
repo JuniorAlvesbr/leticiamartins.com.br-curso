120c75aa95b5fcce5b54e172fab703ca74f3e44e
Junior Alves <jrnalves@gmail.com>
2022-03-30 17:43:16 -0300

    Create subtitle components

---
+++ object/SubTitle/index.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const SubTitle = styled.h2`
+    font-size: 1.3rem;
+    font-weight: 700;
+    text-transform: uppercase;
+    text-align:center;
+`
+
+export const SubTitleH3 = styled(SubTitle)`
+    font-size: 1.1rem;
+`
