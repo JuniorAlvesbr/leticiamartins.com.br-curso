eea6b7d5e2d41d5a023b086ae8dd8dad848088a6
Junior Alves <jrnalves@gmail.com>
2022-04-02 13:42:09 -0300

    Adjuste desktop layout

--- components/AboutMe/styles.js
+++ components/AboutMe/styles.js
@@ -8,7 +8,7 @@ export const Container = styled.section`
     justify-content: center;
     align-items: center;
     background: linear-gradient(180deg, #BF6D8D 9.84%, #000000 42.24%);
-    border-top: 2px solid #000000;
+    border-top: 5px solid #000000;
 
     ${SubTitle} {
         color: ${({ theme }) => theme.colors.primary};
@@ -18,12 +18,12 @@ export const Container = styled.section`
 
     @media (min-width: 750px) {
         flex-direction: row;
-        justify-content: space-evenly;
-        background: linear-gradient(180deg, #BF6D8D 9.84%, #000000 86.24%);
+        justify-content: center;
+        background: linear-gradient(180deg, #BF6D8D 9.84%, #000000 66.24%);
+        padding: 20px;
 
         ${SubTitle} {
             text-align: left;
-            padding-left: 150px;
         }
     }
 `
@@ -45,7 +45,7 @@ export const Image = styled(ImageNext)`
 
 export const Content = styled.article`
     width: 70vw;
-    max-width: 900px;
+    max-width: 1200px;
 `
 
 export const Text = styled.p`
@@ -65,4 +65,9 @@ export const Text = styled.p`
         font-size: 1.2rem;
         padding-right: 80px;
     }
+
+    @media (min-width: 1366px) {
+        font-size: 1.3rem;
+    }
+   
 `
