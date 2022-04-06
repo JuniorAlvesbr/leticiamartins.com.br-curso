bf223bb6995e6cea7e10bd2d08d1eed91625d09c
Junior Alves <jrnalves@gmail.com>
2022-04-05 22:22:07 -0300

    section finished

--- components/Bonus/styles.js
+++ components/Bonus/styles.js
@@ -11,16 +11,67 @@ export const IconSafety = styled(AiOutlineSafetyCertificate)``
 export const IconChat = styled(BsChatLeftText)``
 export const IconCertificate = styled(FaAward)``
 
-export const List = styled.ul``
+export const Container = styled.section`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    
+    & > ${SubTitle} {
+        font-size: 1.1rem;
+        text-align: center;
+        margin: 0 auto;
+        padding-top: 50px;
+    }
+
+    @media (min-width: 700px) {
+        & > ${SubTitle} {
+            font-size: 1.6rem;
+        }
+    }
+`
+
+export const List = styled.ul`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    gap: 20px;
+
+    @media(min-width: 700px) {
+        width: 600px;
+    }
+
+    @media(min-width: 1200px) {
+        width: 100%;
+    }
+`
 
 export const Item = styled.li`
+    width: 250px;
+    height: 200px;
+    display: flex;
+    align-items: center;
     background-color: ${({ theme }) => theme.colors.third};
     border-radius: 10px;
 
-    ${Text} {
-        text-transform: uppercase;
+    & > div {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        width: 100%;
+                
+        & ${IconFactCheck}, ${IconSafety}, ${IconChat}, ${IconCertificate} {
+            color: ${({ theme }) => theme.colors.primary}; 
+            font-size: 3rem 
+        }
 
-        & em {
+        & ${Text} {
+            text-transform: uppercase;
+            width: 200px;
+            text-align: center;
+            line-height: 21px;
+            padding-top: 6px;
+
+            & em {
             color: ${({ theme }) => theme.colors.primary};
         }
 
@@ -28,14 +79,50 @@ export const Item = styled.li`
             text-transform: lowercase;
             font-style: italic;
             font-weight: 400;
+            }
         }
     }
 `
-export const Content = styled.article``
+
+export const Content = styled.article`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-bottom: 24px;
+    gap: 30px;
+
+    & ${ImageWrapper} {
+        width: 280px;
+        height: 290px;
+        margin-bottom: 10px;
+
+        ${Image} {
+            object-fit: contain;
+        }
+    }
+
+    @media(min-width: 700px) {
+        flex-direction: row;
+
+        & ${ImageWrapper} {
+            width: 370px;
+            height: 550px;
+        }
+    }
+
+    @media(min-width: 1200px) {
+        & ${ImageWrapper} {
+            width: 580px;
+            height: 650px;
+        }
+    }
+`
 
 export const TextBox = styled.div`
     background-color: ${({ theme }) => theme.colors.fourth};
     border-radius: 12px;
+    width: 320px;
+    padding: 14px;
 
     & > ${Text} {
         text-transform: uppercase;
@@ -47,132 +134,30 @@ export const TextBox = styled.div`
             font-style: italic;
         }
     }
-`
 
-export const Container = styled.section`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    
-    & > ${SubTitle} {
-        font-size: 1.1rem;
-        text-align: center;
-        margin: 0 auto;
-        padding: 50px;
-    }
-
-    @media (min-width: 700px) {
-        & > ${SubTitle} {
-            font-size: 1.6rem;
-        }
-    }
-
-    & > ${List} {
-        display: flex;
-        flex-wrap: wrap;
-        justify-content: center;
-        gap: 20px;
-
-
-        & ${Item} {
-            display: flex;
-            align-items: center;
-            width: 250px;
-            height: 200px;
-
-            
-            & div {
-                display: flex;
-                flex-direction: column;
-                align-items: center;
-                width: 100%;
-                
-                & ${IconFactCheck}, ${IconSafety}, ${IconChat}, ${IconCertificate} {
-                    color: ${({ theme }) => theme.colors.primary}; 
-                    font-size: 3rem 
-                }
-
-                & ${Text} {
-                    width: 200px;
-                    text-align: center;
-                    line-height: 21px;
-                    padding-top: 6px;
-                }
-
-            }
-        }
-
-        @media(min-width: 700px) {
-            width: 600px;
-        }
-
-        @media(min-width: 1200px) {
-            width: 100%;
-        }
-
-    }
-
-    & > ${Content} {
+    @media(min-width: 700px) {
+        width: 350px;
+        height: 250px;
         display: flex;
         flex-direction: column;
+        justify-content: space-around;
         align-items: center;
-        margin-bottom: 24px;
-        gap: 30px;
 
-        & ${ImageWrapper} {
-            width: 280px;
-            height: 290px;
-            margin-bottom: 10px;
-    
-            ${Image} {
-               object-fit: contain;
-            }
+        ${Text} {
+            font-size: 1.2rem;
+            line-height: 20px;
         }
-    
-        & ${TextBox} {
-            width: 320px;
-            padding: 14px;
-        }
+    }
 
-        @media(min-width: 700px) {
-            flex-direction: row;
+    @media(min-width: 1200px) {
+        width: 503px;
+        height: 350px;
 
-            & ${ImageWrapper} {
-                width: 370px;
-                height: 550px;
-            }
-
-            & ${TextBox} {
-                width: 350px;
-                height: 250px;
-                display: flex;
-                flex-direction: column;
-                justify-content: space-around;
-                align-items: center;
-
-                ${Text} {
-                    font-size: 1.2rem;
-                    line-height: 20px;
-                }
-            }
-        }
-
-        @media(min-width: 1200px) {
-            & ${ImageWrapper} {
-                width: 580px;
-                height: 650px;
-            }
-
-            & ${TextBox} {
-                width: 503px;
-                height: 350px;
-
-                ${Text} {
-                    font-size: 1.5rem;
-                    line-height: 30px;
-                    width: 400px;
-                }
-            }
+        ${Text} {
+            font-size: 1.5rem;
+            line-height: 30px;
+            width: 400px;
         }
     }
 `
+
