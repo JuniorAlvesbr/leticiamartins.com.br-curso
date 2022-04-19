52095bf74f5fe12a4d1736b14bc6f28b73007f3c
Junior Alves <jrnalves@gmail.com>
2022-04-19 10:01:59 -0300

    add font-swap

--- styles/GlobalStyles.js
+++ styles/GlobalStyles.js
@@ -3,7 +3,10 @@ import { createGlobalStyle } from "styled-components";
 const GlobalStyles = createGlobalStyle`
     @font-face {
         font-family: "Autography";
+        font-style: normal;
+        font-weight: 400;
         src: url("/fonts/Autography.otf");
+        font-display: swap;
     }
 
     html, body {
