4988c6344565f4569b0cea8b2c464fb4d18709f4
Junior Alves <jrnalves@gmail.com>
2022-04-22 16:05:08 -0300

    add Robots and sitemap

---
+++ next-sitemap.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+
+module.exports = {
+    siteUrl: 'https://curso.leticiamartins.com.br',
+    generateRobotsTxt: true, // (optional)
+    // ...other options
+}
