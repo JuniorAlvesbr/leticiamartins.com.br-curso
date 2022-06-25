d89724127bc130e61a3354cc177c56056c5f948d
Junior Alves <jrnalves@gmail.com>
2022-06-25 01:54:59 -0300

    show whatsapp scrool

--- components/WhatsAppButton/index.js
+++ components/WhatsAppButton/index.js
@@ -1,21 +1,38 @@
+import { useState, useEffect } from 'react'
 import { WhatsAppIcon, WhatsAppLink } from './styles'
 
 const whatsData = {
   phone: "5544998138138",
   space: "%20",
-  message: function () {
-    return (
-      `Oii${this.space}Leticia,${this.space}tudo${this.space}bem?${this.space}gostaria${this.space}de${this.space}saber${this.space}sobre${this.space}seu${this.space}trabalho${this.space}de...`
-    )
-  }
+  message: ['Oi Leticia. Vi as informações do curso online, mas ainda tenho algumas dúvidas. Pode me ajudar?']
 }
 
-const link = `https://api.whatsapp.com/send?phone=${whatsData.phone}&text=${whatsData.message}`
+const message = whatsData.message.join(`${whatsData.space}`)
+
+const link = `https://api.whatsapp.com/send?phone=${whatsData.phone}&text=${message}`
 
 export default function WhatsAppButton() {
+  const [showWhatsApp, setShowWhatsApp] = useState(false)
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+
+      if (window.scrollY > 400) {
+        setShowWhatsApp(true)
+        return
+      }
+
+      setShowWhatsApp(false)
+    })
+  })
+
   return (
-    <WhatsAppLink href={link} target="_blank">
-      <WhatsAppIcon />
-    </WhatsAppLink>
+    <>
+      {showWhatsApp && (
+        <WhatsAppLink href={link} target="_blank">
+          <WhatsAppIcon />
+        </WhatsAppLink>
+      )}
+    </>
   )
 }
