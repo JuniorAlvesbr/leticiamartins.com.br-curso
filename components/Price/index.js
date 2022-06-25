12095413656e29cfe3e474998338213d83a5b73b
Junior Alves <jrnalves@gmail.com>
2022-06-25 01:54:40 -0300

    change price

--- components/Price/index.js
+++ components/Price/index.js
@@ -9,9 +9,9 @@ export default function Price() {
                 <em>minha casa organizada</em>
             </SubTitle>
 
-            <Text>de <s>R$697,00</s> por apenas 12x</Text>
-            <Text><em>R$</em><strong>39,<span>62</span></strong></Text>
-            <Text><span>ou 397,00 á vista</span></Text>
+            <Text>de <s>R$597,00</s> por apenas 12x</Text>
+            <Text><em>R$</em><strong>29,<span>64</span></strong></Text>
+            <Text><span>ou 297,00 á vista</span></Text>
 
             <Button href="https://pay.hotmart.com/N69221252K?bid=1651088642711" target="_blank">
                 Compre Agora
